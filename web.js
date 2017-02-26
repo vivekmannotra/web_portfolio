710504b0693b1c08395125dbdc07bf715e41d31f
vivekmannotra <vivekmannotra@rocketmail.com>
2017-02-26 08:26:06 +0530

    Update

--- web.js
+++ web.js
@@ -8,27 +8,7 @@ var app = express();
 app.use(express.static(__dirname + '/dist'));
 app.use(bodyParser.json());
 
-//Interceptors
-function requestInterceptor (req, res, next) {
-    if (req.headers['req-auth-token'] !== '#^&SFHAH@Y%$H@$@$%HG$!$#B!$FBWTRBRTHTHRFHBWR@@%$T!SDFEWF@F@F@@ECW') {
-        res.send({
-            status: 'Error',
-            message: 'Not authorised to access the resource.'
-        });
-    } else {
-        next();
-    }
-}
-app.get('/robots.txt', function(req, res){
-    res.sendFile(__dirname + '/dist/robots.txt');
-});
-app.get('/img_dt/:imgName', function(req, res){
-    if (req) {
-
-    }
-    res.sendFile(__dirname + '/images/' + req.params.imgName);
-});
 app.get('/*', function(req, res){
     res.sendFile(__dirname + '/dist/index.html');
 });
-app.listen(5000);
+app.listen(process.env.PORT || 5000);
